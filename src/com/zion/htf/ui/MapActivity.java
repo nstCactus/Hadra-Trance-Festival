065c960929454000b1c851e1c7c86a6144e6fe19
Yohann Bianchi <sbooob@gmail.com>
2013-08-21 02:20:59 +0200

    FIXED: Bug on devices with an outdated Google Play Services lib FIXED: Map reset on orientation change VERSION: bumped to 1.03

--- src/com/zion/htf/ui/MapActivity.java
+++ src/com/zion/htf/ui/MapActivity.java
@@ -19,13 +19,17 @@
 
 package com.zion.htf.ui;
 
+import android.app.Dialog;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
@@ -34,10 +38,13 @@ import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.zion.htf.Application;
@@ -47,64 +54,96 @@ import org.michenux.android.db.sqlite.SQLiteDatabaseHelper;
 
 import java.util.Locale;
 
-public class MapActivity extends SherlockFragmentActivity implements ActionBar.OnNavigationListener{
+public class MapActivity extends SherlockFragmentActivity implements ActionBar.OnNavigationListener, LocationListener{
+	private static final String GOOGLE_MAP_LATITUDE   = "google_map_latitude";
+	private static final String GOOGLE_MAP_LONGITUDE  = "google_map_longitude";
+	private static final String GOOGLE_MAP_ZOOM_LEVEL = "google_map_zoom_level";
+	private static final String GOOGLE_MAP_BEARING    = "google_map_bearing";
+	private static final String GOOGLE_MAP_TILT       = "google_map_tilt";
+	private static final String    GOOGLE_MAP_TYPE       = "google_map_type";
 	private   GoogleMap map;
 	protected int       spinnerPosition;
 	private final String               TAG          = "MapActivity";
 	private       SQLiteDatabaseHelper dbOpenHelper = Application.getDbHelper();
+	private LocationManager locationManager;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_map);
 
-		FragmentManager fragmentManager = this.getSupportFragmentManager();
-		SupportMapFragment supportMapFragment = (SupportMapFragment)fragmentManager.findFragmentById(R.id.map);
-		this.map = supportMapFragment.getMap();
+		// Getting Google Play availability status
+		int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
 
-		// Feature enablement
-		this.map.setMyLocationEnabled(true);
-		this.map.getUiSettings().setCompassEnabled(true);
-		this.map.getUiSettings().setAllGesturesEnabled(true);
-		this.map.getUiSettings().setMyLocationButtonEnabled(true);
-		this.map.getUiSettings().setZoomControlsEnabled(true);
-		this.map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+		// Showing status
+		if(status != ConnectionResult.SUCCESS){ // Google Play Services are not available
+			int requestCode = 10;
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+			dialog.show();
+		}
+		else{
+			SupportMapFragment supportMapFragment = (SupportMapFragment)this.getSupportFragmentManager().findFragmentById(R.id.map);
+			this.map = supportMapFragment.getMap();
 
-		// Initial location
-		this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(45.115137, 5.609024), 16.0f));
+			// Feature enablement
+			this.map.setMyLocationEnabled(true);
+			this.map.getUiSettings().setCompassEnabled(true);
+			this.map.getUiSettings().setAllGesturesEnabled(true);
+			this.map.getUiSettings().setMyLocationButtonEnabled(true);
+			this.map.getUiSettings().setZoomControlsEnabled(true);
+			this.map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
-		// List navigation
-		ActionBar actionBar = this.getSupportActionBar();
-		ArrayAdapter<String> nav = new ArrayAdapter<String>(actionBar.getThemedContext(), R.layout.sherlock_spinner_item, new String[]{
-				this.getString(R.string.action_switchToSatellite),
-				this.getString(R.string.action_switchToTerrain)
-		});
-		nav.setDropDownViewResource(R.layout.sherlock_spinner_dropdown_item);
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-		actionBar.setListNavigationCallbacks(nav, this);
-		actionBar.setHomeButtonEnabled(true);
-		actionBar.setDisplayHomeAsUpEnabled(true);
+			// Configure location polling
+			// Getting LocationManager object from System Service LOCATION_SERVICE
+			this.locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
 
-		// Populate the map with locations
-		try{
-			String langCode = Locale.getDefault().getLanguage().equals("fr") ? "fr" : "en";
-			String query = "SELECT latitude, longitude, label, icon, description FROM locations AS loc INNER JOIN lst__location_types AS lty ON lty.id = loc.location_type AND lty.lang_code = ? LEFT JOIN location_descriptions AS ldc ON ldc.id = loc.location_description AND ldc.lang_code = ?;";
-			Cursor cursor = this.dbOpenHelper.getReadableDatabase().rawQuery(query, new String[]{langCode, langCode});
+			// Creating a criteria object to retrieve provider
+			Criteria criteria = new Criteria();
 
-			while(cursor.moveToNext()){
-				int iconResId = this.getResources().getIdentifier(cursor.getString(3), "drawable", "com.zion.htf");
-				String description = cursor.isNull(4) ? "" : cursor.getString(4);
-				//if(iconResId == 0) iconResId = R.drawable.marker_generic;
-				this.addLocation(new LatLng(cursor.getDouble(0), cursor.getDouble(1)), cursor.getString(2), iconResId, description);
+			// Getting the name of the best provider
+			String provider = this.locationManager.getBestProvider(criteria, true);
+
+			// Getting Current Location
+			Location location = this.locationManager.getLastKnownLocation(provider);
+
+			this.locationManager.requestLocationUpdates(provider, 20000, 0, this);
+
+			// Initial location
+			this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(45.115137, 5.609024), 16.0f));
+
+			// List navigation
+			ActionBar actionBar = this.getSupportActionBar();
+			ArrayAdapter<String> nav = new ArrayAdapter<String>(actionBar.getThemedContext(), R.layout.sherlock_spinner_item, new String[]{
+					this.getString(R.string.action_switchToSatellite),
+					this.getString(R.string.action_switchToTerrain)
+			});
+			nav.setDropDownViewResource(R.layout.sherlock_spinner_dropdown_item);
+			actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+			actionBar.setListNavigationCallbacks(nav, this);
+			actionBar.setHomeButtonEnabled(true);
+			actionBar.setDisplayHomeAsUpEnabled(true);
+
+			// Populate the map with locations
+			try{
+				String langCode = Locale.getDefault().getLanguage().equals("fr") ? "fr" : "en";
+				String query = "SELECT latitude, longitude, label, icon, description FROM locations AS loc INNER JOIN lst__location_types AS lty ON lty.id = loc.location_type AND lty.lang_code = ? LEFT JOIN location_descriptions AS ldc ON ldc.id = loc.location_description AND ldc.lang_code = ?;";
+				Cursor cursor = this.dbOpenHelper.getReadableDatabase().rawQuery(query, new String[]{langCode, langCode});
+
+				while(cursor.moveToNext()){
+					int iconResId = this.getResources().getIdentifier(cursor.getString(3), "drawable", "com.zion.htf");
+					String description = cursor.isNull(4) ? "" : cursor.getString(4);
+					//if(iconResId == 0) iconResId = R.drawable.marker_generic;
+					this.addLocation(new LatLng(cursor.getDouble(0), cursor.getDouble(1)), cursor.getString(2), iconResId, description);
+				}
+				if(!cursor.isClosed()) cursor.close();
 			}
-			if(!cursor.isClosed()) cursor.close();
-		}
-		catch(SQLException e){
-			Toast.makeText(this, this.getString(R.string.error_failed_fetching_poi), Toast.LENGTH_SHORT).show();
-			Log.e(this.TAG, "Impossible de récupérer la liste des POI", e);
-		}
-		finally{
-			this.dbOpenHelper.close();
+			catch(SQLException e){
+				Toast.makeText(this, this.getString(R.string.error_failed_fetching_poi), Toast.LENGTH_SHORT).show();
+				Log.e(this.TAG, "Impossible de récupérer la liste des POI", e);
+			}
+			finally{
+				this.dbOpenHelper.close();
+			}
 		}
 	}
 
@@ -159,5 +198,60 @@ public class MapActivity extends SherlockFragmentActivity implements ActionBar.O
 				ret = false;
 		}
 		return ret;
+	}
+
+	@Override
+	public void onLocationChanged(Location location){
+		this.locationManager.removeUpdates(this);
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras){
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider){
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider){
+
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState){
+		savedInstanceState.putDouble(GOOGLE_MAP_LATITUDE, this.map.getCameraPosition().target.latitude);
+		savedInstanceState.putDouble(GOOGLE_MAP_LONGITUDE, this.map.getCameraPosition().target.longitude);
+		savedInstanceState.putFloat(GOOGLE_MAP_ZOOM_LEVEL, this.map.getCameraPosition().zoom);
+		savedInstanceState.putFloat(GOOGLE_MAP_TILT, this.map.getCameraPosition().tilt);
+		savedInstanceState.putFloat(GOOGLE_MAP_BEARING, this.map.getCameraPosition().bearing);
+		savedInstanceState.putInt(GOOGLE_MAP_TYPE, this.map.getMapType());
+
+		super.onSaveInstanceState(savedInstanceState);
+	}
+
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState){
+		if(null != this.map){
+			double latitude, longitude;
+			float zoom, bearing, tilt;
+			int mapType;
+
+			CameraPosition.Builder cameraPositionBuilder = new CameraPosition.Builder();
+			if(91l != (latitude = savedInstanceState.getDouble(GOOGLE_MAP_LATITUDE, 91l))
+			   && 181l != (longitude = savedInstanceState.getDouble(GOOGLE_MAP_LONGITUDE, 181))) cameraPositionBuilder.target(new LatLng(latitude, longitude));
+
+			if(23 > (zoom = savedInstanceState.getFloat(GOOGLE_MAP_ZOOM_LEVEL, 22))) cameraPositionBuilder.zoom(zoom);
+			if(92 > (tilt = savedInstanceState.getFloat(GOOGLE_MAP_TILT, 91))) cameraPositionBuilder.tilt(tilt);
+			if(362 > (bearing = savedInstanceState.getFloat(GOOGLE_MAP_BEARING))) cameraPositionBuilder.bearing(bearing);
+			if(GoogleMap.MAP_TYPE_SATELLITE == (mapType = savedInstanceState.getInt(GOOGLE_MAP_TYPE))
+			   || GoogleMap.MAP_TYPE_TERRAIN == mapType) this.map.setMapType(mapType);
+
+			this.map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPositionBuilder.build()));
+		}
+
+		super.onRestoreInstanceState(savedInstanceState);
 	}
 }
